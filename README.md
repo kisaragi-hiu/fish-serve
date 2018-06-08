d44e96cf99b466d217161830de28b0fee7546174
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-06-08 18:09:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# serve
+
+Launch a server in a directory.
+
+Uses Python's `http.server` module.
+
+## Install
+
+With [fisherman](//github.com/fisherman/fisherman):
+
+```fish
+fisher kisaragi-hiu/fish-serve
+```
+
+## Usage
+
+```fish
+serve 8080 ~/public/
+```
---
+++ functions/serve.fish
@@ -0,0 +1,15 @@
+function serve
+    # serve port [path]
+    if test -n "$argv[1]"
+        if test -n "$argv[2]"
+            pushd "$argv[2]"
+        end
+        python -m http.server $argv[1]
+        if test -n "$argv[2]"
+            popd
+        end
+    else
+        python -m http.server 4000
+    end
+end
+
